bd2baabf498d6512ce0fbde3ddf5f0241fc29b3a
frizadiga <frizadiga@gmail.com>
2019-02-09 17:53:26 +0700

    Add dynamic import

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,15 +3,16 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './app.scss';
 
 // Components
-import Menu from '../menu';
+import Loading from '../loading';
 import BottomBar from '../bottom-bar';
-import NotFoundPage from '../not-found-page';
-import HomePage from '../home-page';
-import LoginPage from '../login-page';
-import UserPage from '../user-page';
-import PostsDetailPage from '../posts-detail-page';
-import PhotosPage from '../photos-page';
-import PhotosDetailPage from '../photos-detail-page';
+const Menu = React.lazy(() => import('../menu'));
+const NotFoundPage = React.lazy(() => import('../not-found-page'));
+const HomePage = React.lazy(() => import('../home-page'));
+const LoginPage = React.lazy(() => import('../login-page'));
+const UserPage = React.lazy(() => import('../user-page'));
+const PostsDetailPage = React.lazy(() => import('../posts-detail-page'));
+const PhotosPage = React.lazy(() => import('../photos-page'));
+const PhotosDetailPage = React.lazy(() => import('../photos-detail-page'));
 
 const App = () => {
   return (
@@ -19,17 +20,20 @@ const App = () => {
       <main className="app">
         <Router basename={process.env.PUBLIC_URL}>
           <div className="app__router-view">
-            <Switch>
-              <Route key="1" path="/" component={HomePage} exact/>
-              <Route key="2" path="/menu" component={Menu} exact/>
-              <Route key="3" path="/login" component={LoginPage} exact/>
-              <Route key="4" path="/users/:id" component={UserPage} exact/>
-              <Route key="5" path="/posts/:id" component={PostsDetailPage} exact/>
-              <Route key="6" path="/photos-list/:id" component={PhotosPage} exact/>
-              <Route key="7" path="/photos-detail/:id" component={PhotosDetailPage} exact/>
-              <Route key="0" component={NotFoundPage} />
-            </Switch>
-            { true && <BottomBar /> }
+            <React.Suspense fallback={Loading}>
+              <Switch>
+                <Route key="1" path="/" component={HomePage} exact/>
+                <Route key="2" path="/menu" component={Menu} exact/>
+                <Route key="3" path="/login" component={LoginPage} exact/>
+                <Route key="4" path="/users/:id" component={UserPage} exact/>
+                <Route key="5" path="/posts/:id" component={PostsDetailPage} exact/>
+                <Route key="6" path="/photos-list/:id" component={PhotosPage} exact/>
+                <Route key="7" path="/photos-detail/:id" component={PhotosDetailPage} exact/>
+                <Route key="100" path="/loading" component={Loading} exact/>
+                <Route key="0" component={NotFoundPage} />
+              </Switch>
+            </React.Suspense>
+            <BottomBar />
           </div>
         </Router>
       </main>
