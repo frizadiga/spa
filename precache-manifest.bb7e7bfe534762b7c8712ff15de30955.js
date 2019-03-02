6f9848337578917981ab0af81ed3e72d81e61383
frizadiga <frizadiga@gmail.com>
2019-03-02 15:20:37 +0000

    Automated deployment to GitHub Pages: 3f611cc60435d413cc6b26a498917dc28e891db2

---
+++ precache-manifest.bb7e7bfe534762b7c8712ff15de30955.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "987250bc5181b3c6838b",
+    "url": "/spa/static/css/7.c0968528.chunk.css"
+  },
+  {
+    "revision": "98a4bff76780d411c59e",
+    "url": "/spa/static/js/0.98a4bff7.chunk.js"
+  },
+  {
+    "revision": "646a8ae042b1e18e9202",
+    "url": "/spa/static/js/main.646a8ae0.chunk.js"
+  },
+  {
+    "revision": "486a18f208c5509b737c",
+    "url": "/spa/static/js/runtime~main.486a18f2.js"
+  },
+  {
+    "revision": "e38d328eaab6a41bd41e",
+    "url": "/spa/static/js/3.e38d328e.chunk.js"
+  },
+  {
+    "revision": "f5157cd80e72b0cd9839",
+    "url": "/spa/static/js/11.f5157cd8.chunk.js"
+  },
+  {
+    "revision": "e02c4e36da614d71bf5c",
+    "url": "/spa/static/js/4.e02c4e36.chunk.js"
+  },
+  {
+    "revision": "e691effb6b8238b49f92",
+    "url": "/spa/static/js/10.e691effb.chunk.js"
+  },
+  {
+    "revision": "165a9e91da843501c628",
+    "url": "/spa/static/js/5.165a9e91.chunk.js"
+  },
+  {
+    "revision": "c6bc1e0b70d6a29cab68",
+    "url": "/spa/static/js/9.c6bc1e0b.chunk.js"
+  },
+  {
+    "revision": "41be877348467476ecd4",
+    "url": "/spa/static/js/6.41be8773.chunk.js"
+  },
+  {
+    "revision": "e91dc6d50cdb885c2504",
+    "url": "/spa/static/js/8.e91dc6d5.chunk.js"
+  },
+  {
+    "revision": "987250bc5181b3c6838b",
+    "url": "/spa/static/js/7.987250bc.chunk.js"
+  },
+  {
+    "revision": "646a8ae042b1e18e9202",
+    "url": "/spa/static/css/main.c1e4ae13.chunk.css"
+  },
+  {
+    "revision": "c6bc1e0b70d6a29cab68",
+    "url": "/spa/static/css/9.76b33520.chunk.css"
+  },
+  {
+    "revision": "e91dc6d50cdb885c2504",
+    "url": "/spa/static/css/8.af7afe70.chunk.css"
+  },
+  {
+    "revision": "41be877348467476ecd4",
+    "url": "/spa/static/css/6.c8809331.chunk.css"
+  },
+  {
+    "revision": "165a9e91da843501c628",
+    "url": "/spa/static/css/5.845e8bad.chunk.css"
+  },
+  {
+    "revision": "e02c4e36da614d71bf5c",
+    "url": "/spa/static/css/4.19fa0f10.chunk.css"
+  },
+  {
+    "revision": "f5157cd80e72b0cd9839",
+    "url": "/spa/static/css/11.2261335f.chunk.css"
+  },
+  {
+    "revision": "e691effb6b8238b49f92",
+    "url": "/spa/static/css/10.9010485e.chunk.css"
+  },
+  {
+    "revision": "cfc6eba6704cdf8236c6e6324f4c03c0",
+    "url": "/spa/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/spa/precache-manifest.7f34538e04b3184089f9e9c53e11cd0a.js"
+  "/spa/precache-manifest.bb7e7bfe534762b7c8712ff15de30955.js"
 );
 
 workbox.clientsClaim();
